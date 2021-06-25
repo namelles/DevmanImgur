7ad7c06545924aef08b30dd5f17babbeafefa61d
noname <baev_aleksei@mail.ru>
2021-06-25 14:56:08 +0300

    Adding a variable maximum image size

--- сommon_functions.py
+++ сommon_functions.py
@@ -1,8 +1,9 @@
 from PIL import Image
 
 def change_size_mode_image(image_path):
+    max_size_image = (1800, 1800)
     image = Image.open(image_path)
-    image.thumbnail((1800, 1800))
+    image.thumbnail(max_size_image)
     if image.mode in ["RGBA", "P"]:
         image = image.convert('RGB')
     image.save(f'{image_path}', format('JPEG'))
