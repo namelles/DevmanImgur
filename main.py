6b61184340bd80b55aac174a9a3f9f3ed2c4c59f
noname <baev_aleksei@mail.ru>
2021-06-27 14:13:30 +0300

    Rename variable

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def authenticate_imgur(client_id, client_secret):
 
 def upload_images_imgur(client, img_catalog_path):
     album = None
-    list_images = listdir(img_catalog_path)
-    images = filter(lambda x: x.endswith('.jpg'), list_images)
+    name_images = listdir(img_catalog_path)
+    images = filter(lambda x: x.endswith('.jpg'), name_images)
     for image in images:
         config = {
             'album': album,
