eb20f4a3fb952b608d3c764cc11b5d67a07d44e2
noname <baev_aleksei@mail.ru>
2021-07-08 13:53:05 +0300

    Removed the qualifier from the variable name.

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -13,14 +13,14 @@ def download_spacex_image(url, img_catalog_path):
         file.write(response.content)
 
 
-def get_spacex_images_urls(spacex_start_number):
-    url = f'https://api.spacexdata.com/v3/launches/{spacex_start_number}'
+def get_spacex_images_urls(start_number):
+    url = f'https://api.spacexdata.com/v3/launches/{start_number}'
     response = requests.get(url)
     response.raise_for_status()
     return response.json()[0]['links']['flickr_images']
 
 
-def download_spacex_launch_images(spacex_starts_number, img_catalog_path):
-    spacex_image_urls = get_spacex_images_urls(spacex_starts_number)
+def download_spacex_launch_images(starts_number, img_catalog_path):
+    spacex_image_urls = get_spacex_images_urls(starts_number)
     for spacex_image_url in spacex_image_urls:
         download_spacex_image(spacex_image_url, img_catalog_path)
